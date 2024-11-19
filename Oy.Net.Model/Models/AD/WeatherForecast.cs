21395ee5e9e3fcc643fe40c25b60ce06140ef5bd
Oscar <a1783709064@163.com>
2024-11-19 22:48:49 +0800

    CREATE MENU
    Signed-off-by: Oscar <a1783709064@163.com>

---
+++ Oy.Net.Model/Models/AD/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Oy.Net.Model.Models.AD;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
